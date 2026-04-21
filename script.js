63860a2b6992d786aa1d4adf58a365a29f8ac9de
lintangshafaranisa <shafaranisalintang@gmail.com>
2026-04-21 19:06:26 +0700

    update

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const close = document.querySelector(".close");
   const judulModal = document.getElementById("editModal");
 
-  let layanan = JSON.parse(localStorage.getItem("layanan")) || [];
+  let layanan = JSON.parse(localStorage.getItem("layanan")) || [
+  { kode: "A001", nama: "Potong Rambut", harga: 50000 },
+  { kode: "A002", nama: "Cat Rambut", harga: 150000 },
+  { kode: "A003", nama: "Hair Spa", harga: 100000 },
+  { kode: "A004", nama: "Spa Badan", harga: 250000 },
+  { kode: "A005", nama: "Pedicure & Manicure", harga: 300000 },
+  { kode: "A006", nama: "Facial Wajah", harga: 200000 },
+  { kode: "A007", nama: "Creambath", harga: 140000 }
+];
+  localStorage.setItem("layanan", JSON.stringify(layanan));
   let editIndex = -1;
 
   // ================= MODAL =================
